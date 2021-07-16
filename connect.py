32aab1cb3d7c135d0338bac6ce418d8674039ce9
Debrup Panda <79893783+Debrup-commits@users.noreply.github.com>
2021-07-16 12:39:16 +0530

    Add files via upload

---
+++ connect.py
@@ -0,0 +1,44 @@
+import numpy as np
+import cv2
+
+img = cv2.imread('image.PNG')
+
+#converting to BnW
+img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+#thresholding
+ret, thresh = cv2.threshold(img_gray, 240, 255, 0)
+
+#finding and printing no.of contours
+contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+print(f"NUmber of contours = {str(len(contours))}")
+
+#drawing contours
+cv2.drawContours(img, contours, -1, (255, 255, 255), 1)
+
+def identify_centroid(image, centroid):
+    cent_moment = cv2.moments(centroid)
+    centroid_x = int(cent_moment['m10']/cent_moment['m00'])
+    centroid_y = int(cent_moment['m01']/cent_moment['m00'])
+
+    #cv2.circle(img, (centroid_x, centroid_y), 10, (255, 0, 0), -1)
+    return image
+
+for (i, c) in enumerate(contours):
+    orig = identify_centroid(img, c)
+    
+img = cv2.line(img, (141, 131), (516, 551), (0, 0, 0), 1)
+img = cv2.line(img, (516, 551), (666, 355), (0, 0, 0), 1)
+img = cv2.line(img, (666, 355), (909, 257), (0, 0, 0), 1)
+img = cv2.line(img, (909, 257), (366, 295), (0, 0, 0), 1)
+img = cv2.line(img, (366, 295), (161, 571), (0, 0, 0), 1)
+img = cv2.line(img, (161, 571), (674, 138), (0, 0, 0), 1)
+img = cv2.line(img, (674, 138), (909, 537), (0, 0, 0), 1)
+cv2.imshow('Image', img)
+cv2.imshow('BnW', img_gray)
+cv2.imshow('thresholded', thresh)
+
+cv2.imwrite('Output.jpg', img)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows
